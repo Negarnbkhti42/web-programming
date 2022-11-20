e94c597052a76930fca90c488d456e885ac6c51f
Negarnbkhti42 <norob35@gmail.com>
2022-11-20 16:22:21 +0330

    Add filter utilities

--- utils.js
+++ utils.js
@@ -29,3 +29,30 @@ export const CITIES = [
   "کرمان",
   "کرمانشاه",
 ];
+
+const TICKETS = [];
+
+function filterTicketsBySource(list, source) {
+  return list.filter((ticket) => ticket.source == source);
+}
+
+function filterTicketsByDestination(list, dest) {
+  return list.filter((ticket) => ticket.destination == dest);
+}
+
+function filterTicketsByClass(list, classes) {
+  return list.filter((ticket) => classes.includes(ticket.class));
+}
+
+function filterTicketsByPrice(list, maxPrice) {
+  return list.filter((ticket) => ticket.price <= maxPrice);
+}
+
+function filterTickets({ source, dest, classes, maxPrice }) {
+  let filteredTickets = filterTicketsBySource(TICKETS, source);
+  filteredTickets = filterTicketsByDestination(filteredTickets, dest);
+  filteredTickets = filterTicketsByClass(filteredTickets, classes);
+  filteredTickets = filterTicketsByPrice(filteredTickets, maxPrice);
+
+  return filteredTickets;
+}
