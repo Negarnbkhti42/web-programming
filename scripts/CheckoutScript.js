90afe4766ec17acec0fd5a3f53a65f152db364a1
Negarnbkhti42 <norob35@gmail.com>
2022-12-05 11:52:20 +0330

    Get first passenger info

--- scripts/CheckoutScript.js
+++ scripts/CheckoutScript.js
@@ -1,9 +1,27 @@
+const initialForm = document.getElementById("firstPassengerForm");
 const form = document.getElementById("passengerForm");
 
-form.addEventListener("submit", (event) => {
-  if (!form.checkValidity()) {
-    event.preventDefault();
-    event.stopPropagation();
+const firstSection = document.getElementById("firstPassenger");
+const extraSection = document.getElementById("extraPassenger");
+
+initialForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (!initialForm.checkValidity()) {
+    event.stopPropagation;
+    initialForm.classList.add("was-validated");
+  } else {
+    firstSection.hidden = true;
+    extraSection.hidden = false;
   }
-  form.classList.add("was-validated");
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (!form.checkValidity()) {
+    event.stopPropagation();
+    form.classList.add("was-validated");
+  } else {
+    console.log(event);
+  }
 });
